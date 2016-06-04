a033d413b12ba00254667a774996c84c8248100e
Tom van Rooij <djtomliempde@hotmail.com>
2016-06-04 14:06:29 +0200

    Blink the Philips Hue lights
    replace SSID, PASSWORD, BRIDGE_IP, USERNAME

---
+++ examples/blink/blink.ino
@@ -0,0 +1,45 @@
+#include <ESP8266WiFi.h>
+#include <ESPHue.h>
+
+const char* ssid = "SSID";    // your network SSID (name)
+const char* pass = "PASSWORD";    // your network password
+
+//                 bridge ip    username
+ESPHue h = ESPHue("BRIDGE_IP", "USERNAME");
+
+void setup() {
+  Serial.begin(115200);
+  connectToWifi();
+}
+
+void loop() {
+  h.HSB(2000, 255, 255); //configure hsb
+  h.set(1); //apply set hsb to lamp 1
+  h.set(2); //apply set hsb to lamp 2
+
+  delay(1000);
+
+  h.off(); //configure off
+  h.set(1); //apply set off to lamp 1
+  h.set(2); //apply set off to lamp 2
+
+  delay(1000);
+}
+
+void connectToWifi() {
+  Serial.println();
+  Serial.println();
+  Serial.print(F("Attempting to connect to network: "));
+  Serial.println( ssid );
+
+  uint8_t timeout = 0;
+  WiFi.begin( ssid, pass );
+  while( WiFi.status() != WL_CONNECTED && timeout < 250) {
+    delay(250);
+    Serial.print('.');
+    timeout++;
+  }
+  Serial.println();
+  if( timeout >= 250 ) Serial.println(F("Failed to connect to Wifi!"));
+  else Serial.println(F("Connected to wifi"));
+}
